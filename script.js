65ee378c8aa1f2207fd3a7849b1c696300d509a9
Adam Page <63195949+adampage1993@users.noreply.github.com>
2020-10-13 20:09:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ window.addEventListener('scroll', function () {
   let windowPosition = window.scrollY > 10;
   navbar.classList.toggle('sticky', windowPosition);
 });
+
+function navSlide() {
+  const burger = document.querySelector('.menu-btn');
+  const nav = document.querySelector('.navbar .menu');
+  const burgerIcon = document.querySelector('.menu-btn i');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burgerIcon.classList.toggle('active');
+  });
+}
+
+navSlide();
