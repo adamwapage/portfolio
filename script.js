4a6fe727a420e80797e690fefa9e3885fa96efc4
Adam Page <63195949+adampage1993@users.noreply.github.com>
2020-10-20 08:28:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ function navSlide() {
     nav.classList.toggle('active');
     burgerIcon.classList.toggle('active');
   });
+  const link1 = document.querySelector('.navbar .menu li a');
+  const link2 = document.querySelectorAll('.navbar .menu li a')[1];
+  const link3 = document.querySelectorAll('.navbar .menu li a')[2];
+  link1.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burgerIcon.classList.toggle('active');
+  });
+  link2.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burgerIcon.classList.toggle('active');
+  });
+  link3.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burgerIcon.classList.toggle('active');
+  });
 }
 
 navSlide();
