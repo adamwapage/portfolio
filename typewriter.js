0dabdf0e24caf21372e937e975fd6dda7b1a1fc9
Adam Page <adampage1993@gmail.com>
2021-03-13 17:26:39 +0000

    add typewriter effect

---
+++ typewriter.js
@@ -0,0 +1,46 @@
+const textDisplay = document.querySelector('.text-3');
+const phrases = ["And I'm a web developer.", 'And I love to code.', 'And I love to learn.'];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop() {
+  isEnd = false;
+  textDisplay.innerHTML = currentPhrase.join('');
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.innerHTML = currentPhrase.join('');
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      textDisplay.innerHTML = currentPhrase.join('');
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i === phrases.length) {
+        i = 0;
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 200;
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+  setTimeout(loop, time);
+}
+
+loop();
